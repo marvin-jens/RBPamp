1fc985c195e77dbb5c710a7ec48913b69f675b27
Marvin Jens <mjens@mit.edu>
2017-03-17 12:04:29 -0400

    * make OpenenHistCollection use @pickled, @cached from the caching framework

--- cska/folding.py
+++ cska/folding.py
@@ -2,37 +2,61 @@
 import sys
 import numpy as np
 from collections import defaultdict
+import cPickle as pickle
+from cska.caching import CachedBase, cached, pickled
+import logging
 
-class OpenenAggregator(object):
-    def __init__(self, min_en=0., max_en=20., n_bins=100, n_chunk=10000):
+class OpenenHistCollection(CachedBase):
+    def __init__(self, name="openen.hist", min_en=0., max_en=20., n_bins=100, n_chunk=100):
+        
+        CachedBase.__init__(self)
+        
+        self.name = name
         step = (max_en-min_en)/n_bins
         self.bins = np.arange(min_en, max_en+step, step)
         self.bins[-1] = np.inf
         self.n_chunk = n_chunk
         self.kmer_raw = defaultdict(list)
-        self.kmer_binned = defaultdict(lambda : np.zeros(n_bins, dtype=int))
+        self.kmer_binned = {}
         self.n_raw = 0
-
+        self.logger = logging.getLogger("OpenenHistCollection({0})".format(self.name) )
+    
+    @property
+    def cache_key(self):
+        return self.name
+    
     def add_chunk(self, chunk):
         for kmer, openen in chunk.openen:
             self.kmer_raw[kmer].append(openen)
         self.n_raw += 1
         
-        if self.n_raw >= self.n_chunk:
-            self.process(min_data=100)
+        if self.n_raw and (self.n_raw % self.n_chunk == 0):
+            self.digest(min_data=10, _do_not_unpickle=True, _do_not_cache=True)
             self.n_raw = 0
     
-    def process(self, min_data=0):
+    @cached
+    @pickled
+    def digest(self, min_data=0):
+        self.logger.debug("digest() at n_raw={0}".format(self.n_raw) )
         for kmer, data in self.kmer_raw.items():
             if len(data) > min_data:
                 binned, bins = np.histogram(np.array(data), bins=self.bins)
-                self.kmer_binned[kmer] += binned
+                if not kmer in self.kmer_binned:
+                    self.kmer_binned[kmer] = binned
+                else:
+                    self.kmer_binned[kmer] += binned
+                
+                # release memory
                 self.kmer_raw[kmer] = []
         
-        print self.kmer_binned['GCATG']
         return self.kmer_binned
 
+    @cached
+    def __getitem__(self, kmer):
+        return self.digest()[kmer]
+
     def aggregate_from_file(self, src):
+        self.logger.debug("aggregating data from {0}".format(src) )
         chunk = RNAplfoldChunk()
         for line in src:
             if line.startswith('>'):
@@ -45,8 +69,9 @@ class OpenenAggregator(object):
                 chunk.add_line(line)
                 
         self.add_chunk(chunk)
-        return self.process()
-        
+        return self.digest()
+
+    
 class RNAplfoldChunk(object):
     def __init__(self):
         self.n = 0
@@ -66,6 +91,9 @@ class RNAplfoldChunk(object):
             self.openen.append( (kmer, float(col) ) )
 
 
-
-oa = OpenenAggregator()
-print oa.aggregate_from_file(sys.stdin)['GCATG']
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    oa = OpenenHistCollection()
+    oa.aggregate_from_file(sys.stdin)
+    print oa['TGCATGT']
+             
