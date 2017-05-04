09b151376208ee67991cc58883b19507ba4ecb3f
Marvin Jens <mjens@mit.edu>
2017-05-04 17:10:42 -0400

    * cleaned up command line interface. Started adding support for rbns model fitting and simulations

--- cska/__init__.py
+++ cska/__init__.py
@@ -27,32 +27,43 @@ def main():
     usage = "usage: %prog [options] <input_reads_file> <pulldown_reads_file1> [<pulldown_reads_file2] [...]"
 
     parser = OptionParser(usage=usage)
+    parser.add_option("","--name",dest="name",default="RBP",help="name of the protein assayed (default=RBP)")
+    parser.add_option("-o","--output",dest="output",default=".",help="path where results are to be stored")
+    parser.add_option("","--compute-results",dest="results",default="R_values,SKA_weights,F_ratios",help="list of RBNS metrics to compute and store (default='R_values,SKA_weights,F_ratios')")
+    parser.add_option("","--interactions",dest="interactions",default=False, action="store_true",help="SWITCH: activate combinatorial search") # TODO: merge into --compute-results
+    parser.add_option("","--debug",dest="debug",default=False, action="store_true",help="SWITCH: activate debug output")
+    parser.add_option("","--version",dest="version",default=False, action="store_true",help="show version information and quit")
+
     parser.add_option("-k","--min-k",dest="min_k",default=3,type=int,help="min kmer size (default=3)")
     parser.add_option("-K","--max-k",dest="max_k",default=8,type=int,help="max kmer size (default=8)")
     
-    parser.add_option("","--ska-max-passes",dest="n_passes",default=10,type=int,help="max number of passes (default=10)")
     parser.add_option("-r","--rna-concentration",dest="rna_conc",default=1000.,type=float,help="concentration of random RNA used in the experiment in micro molars (default=100uM)")
     parser.add_option("-p","--rbp-concentration",dest="prot_conc",default="0,320",help="(comma separated list of) protein concentration used in the experiment(s) in nano molars (default=0,300)")
     parser.add_option("-T","--temperature",dest="temp",default=22,type=float,help="temperature of the experiment in Celsius (default=22.0)")
-    parser.add_option("","--name",dest="name",default="RBP",help="name of the protein assayed (default=RBP)")
     parser.add_option("","--subsamples",dest="subsamples",default=10,type=int,help="number of subsamples for error estimation (default=10)")
     parser.add_option("","--pseudo",dest="pseudo",default=10.,type=float,help="pseudo count to add to kmer counts in order to avoid div by zero for large k (default=10)")
+    parser.add_option("","--ska-max-passes",dest="n_passes",default=10,type=int,help="max number of passes (default=10)")
     parser.add_option("","--ska-convergence",dest="convergence",default=0.5,type=float,help="convergence is reached when max. change in absolute weight is below this value (default=0.5)")
-    parser.add_option("-o","--output",dest="output",default=".",help="path where results are to be stored")
-    parser.add_option("","--compute-results",dest="results",default="R_values,SKA_weights,F_ratios",help="list of RBNS metrics to compute and store (default='R_values,SKA_weights,F_ratios')")
-    parser.add_option("","--debug",dest="debug",default=False, action="store_true",help="SWITCH: activate debug output")
-    parser.add_option("","--interactions",dest="interactions",default=False, action="store_true",help="SWITCH: activate combinatorial search")
-    parser.add_option("-n","--n-max",dest="n_max",default=0, type=int,help="TESTING: read at most N reads")
+
     parser.add_option("","--disable-caching",dest="disable_caching",default=False, action="store_true",help="DEBUG: disable transparent caching (SLOW!)")
     parser.add_option("","--disable-unpickle",dest="disable_unpickle",default=False, action="store_true",help="DEBUG: disable unpickling. Will recompute and overwrite existing pickled data")
     parser.add_option("","--disable-pickle",dest="disable_pickle",default=False, action="store_true",help="DEBUG: disable pickling. Will not create or overwrite any pickled data")
     parser.add_option("","--debug-caching",dest="debug_caching",default=False, action="store_true",help="DEBUG: enable detailed debug output from the caching framework")
-    parser.add_option("","--openen",dest="folding",default=False, action="store_true",help="SWITCH: instead of a normal run, fold all reads and built open-energy distributions")
-    parser.add_option("","--version",dest="version",default=False, action="store_true",help="show version information and quit")
-    parser.add_option("-w","--write-fasta",dest="write_fasta",default=False, action="store_true",help="SWITCH: write FASTA file for each library with isolated, top-scoring kmers")
-    parser.add_option("","--adap-5",dest="adap5",default="gggaguucuacaguccgacgauc", help="5'RNA adapter sequence to add to FASTA ouput")
-    parser.add_option("","--adap-3",dest="adap3",default="uggaauucucgggugucaagg", help="3'RNA adapter sequence to add to FASTA ouput")
+
+    parser.add_option("-n","--n-max",dest="n_max",default=0, type=int,help="TESTING: read at most N reads")
+    parser.add_option("","--adap-5",dest="adap5",default="gggaguucuacaguccgacgauc", help="5'RNA adapter sequence to add to read sequence")
+    parser.add_option("","--adap-3",dest="adap3",default="uggaauucucgggugucaagg", help="3'RNA adapter sequence to add to read sequence")
+    parser.add_option("","--openen",dest="folding",default=False, action="store_true",help="SWITCH: instead of a normal run, fold all reads and build open-energy distributions")
     parser.add_option("","--parallel",dest="parallel",default=8,type=int,help="number of parallel threads (currently only used for folding. default=8)")
+    parser.add_option("","--known-kd",dest="known_kd",default="", help="CSKA reads known dissociation constants for kmers from this file (format: <kmer>\t<Kd_in_nM>).")
+
+    parser.add_option("","--simulate",dest="simulate",choices=["","reads","comparison"],default="",help="simulate RBNS instead of analysis, choices are ['reads','comparison']")    
+    parser.add_option("","--seed",dest="seed",default=47110815,type=int,help="seed for fast pseudo-random number generator (for RBNS simulation)")
+    parser.add_option("","--sim-best-Kd",dest="sim_best_Kd",default=10.,type=float,help="best binding dissociation constant for simulation in nM (default=10 nM)")
+    parser.add_option("","--sim-var",dest="sim_var",default=10.,type=float,help="variance for simulated binding energy log-normal distribution (default=)")
+    parser.add_option("","--sim-mean",dest="sim_mean",default=10.,type=float,help="mean for simulated binding energy log-normal distribution (default=)")
+    parser.add_option("","--sim-N-reads",dest="sim_N_reads",default=1000000,type=int,help="number of reads to simulate (default=1,000,000)")
+    
     options,args = parser.parse_args()
 
     if options.version:
@@ -95,7 +106,6 @@ def main():
     logger.info("version {0}".format(__version__))
     logger.info("invoked as '{0}'".format(" ".join(sys.argv)) )
 
-
     # parametrize SKA algorithm
     ska = SKARunner(
         max_iterations = options.n_passes,
@@ -110,8 +120,27 @@ def main():
         rbp_name = options.name,
         out_path = options.output,
         ska_runner = ska,
-        write_fasta = options.write_fasta,
+        known_kd = options.known_kd,
     )
+
+    # TODO: properly integrate simulation
+    if options.simulate == "reads":
+        from cska.rbns_model import RBNSGenerator
+        for k in range(options.min_k, options.max_k + 1):
+            gen = RBNSGenerator(k,l=40, seed=options.seed)
+            gen.assign_experimental_input(args[0])
+            gen.energy_plot()
+
+            r_matrix = []
+            for P in np.array(rbp_concentrations[1:]):
+                r = gen.predict_r_values(P=P, store="r_{0}.tsv".format(P))
+                r_matrix.append(r)
+                
+                read_path = os.path.join(self.out_path,"sim_bound_{0}.reads".format(P) )
+                gen.generate_bound_reads(read_path, P=P, p_ns=0.00, N=20000000)
+                occ = gen.predict_occupancies(P=P, store="occ_{0}.tsv".format(P))
+                
+
     
     # populate with experimental data
     for fname, rbp_conc in zip(args, rbp_concentrations):
@@ -128,6 +157,7 @@ def main():
         )
         
         rbns.add_reads(reads)
+
 
     fold_path = os.path.join(options.output,"openen")
     if options.folding:
