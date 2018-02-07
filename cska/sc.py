f868454182c799945291b1a1ef08c46e2c4385cd
Marvin Jens <mjens@mit.edu>
2018-02-07 14:41:02 -0500

    * implemented approximation to self-consistent protein concentration optimization. Instead optimizing over all sequences, bin them by Z (part. function) and opt. over histogram

--- cska/sc.py
+++ cska/sc.py
@@ -11,17 +11,33 @@ class SelfConsistency(object):
         self.rna_conc = rna_conc
         
         if bins:
+            # logarithmic binning
             self.Z1.sort()
             lZ = np.log(Z1)
-            self.counts, self.bins = np.histogram(lZ, bins=bins)
-            self.bins = np.exp(self.bins)
-            print self.bins
-            print Z1.sum()
-            print np.trapz(self.counts, self.bins[1:])
+            self.counts, bins = np.histogram(lZ, bins=bins)
+            self.bins = np.exp(bins)
+            # midpoint integration
+            self.x = (self.bins[1:] + self.bins[:-1])/2.
+            
+            # switch over to fast approximation
+            self.free_rbp = self.fast_free_rbp
 
     def fast_free_rbp(self, rbp_total):
         # TODO: use the binned version. Compare accuracy!
-        pass
+        t0 = time.time()
+        def to_optimize(p_free):
+            Z = p_free * self.x
+            p = Z / (Z + 1.)
+            
+            rbp_bound = ((p * self.rna_conc)*self.counts).sum() / self.N
+            return ((rbp_total - rbp_bound) - p_free)**2
+            
+        res = minimize_scalar(to_optimize, bounds = (0, rbp_total), method='Bounded')
+        t1 = time.time()
+        perc = 100. * res.x / rbp_total
+        self.logger.debug("total={0:.1f} free={1:.1f} ({2:.2f}%) in {3:.2f} ms".format(rbp_total, res.x, perc, 1000*(t1-t0)) )
+        
+        return res.x
         
     def free_rbp(self, rbp_total):
     
@@ -36,7 +52,7 @@ class SelfConsistency(object):
             
         res = minimize_scalar(to_optimize, bounds = (0, rbp_total), method='Bounded')
         t1 = time.time()
-        perc = res.x / rbp_total
+        perc = 100. * res.x / rbp_total
         self.logger.debug("total={0:.1f} free={1:.1f} ({2:.2f}%) in {3:.2f} ms".format(rbp_total, res.x, perc, 1000*(t1-t0)) )
         
         return res.x
@@ -47,6 +63,9 @@ class SelfConsistency(object):
         #return np.array(rbp_free, dtype= np.float32)
     
 if __name__ == "__main__":
-    Z1 = np.load('/scratch/data/RBNS/RBFOX3/new/opt/5mers/5mer_Z1.npy')
-    sc = SelfConsistency(Z1, 1000.)
+    logging.basicConfig()
+    logging.getLogger('model').setLevel(logging.DEBUG)
+    Z1 = np.load('/scratch/data/RBNS/RBFOX3/new/opt/5mers/5mer_Z1_.npy')
+    sc = SelfConsistency(Z1, 1000., bins=10000)
     print 5, "->", sc.free_rbp(5.)
+    print 5, "->", sc.fast_free_rbp(5.)
