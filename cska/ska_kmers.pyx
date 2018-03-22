2424730cabde7c1f6c23be04908a10e66d3884da
Marvin Jens <mjens@mit.edu>
2018-03-22 16:47:17 -0400

    * fixed a bug in read_raw_seqs_chunked that would add a chunk even though nothing had been read at all.

--- cska/ska_kmers.pyx
+++ cska/ska_kmers.pyx
@@ -402,9 +402,12 @@ def read_raw_seqs_chunked(src, str pre="", str post="", UINT32_t n_max=0, UINT32
         if not L:
             L = len(line)
             chunkbytes = chunklines*L
+            # print "discovered L=",L
         
         if N % chunklines == 0:
-            if N: chunks.append(_buf)
+            if n: 
+                # print "new chunk at", N, n
+                chunks.append(_buf)
             _buf = np.empty(L*chunklines, dtype=np.uint8)
             buf = _buf # initialize the view
             n = 0
@@ -428,8 +431,10 @@ def read_raw_seqs_chunked(src, str pre="", str post="", UINT32_t n_max=0, UINT32
         if N >= n_max + n_skip and n_max:
             break
 
+    # print "N",N, len(chunks), "chunks", n, "left in _buf"
     chunks.append(_buf[:n])
     cat = np.concatenate(chunks)
+    # print cat.shape
 
     return cat.reshape((N,L))
 
@@ -974,6 +979,59 @@ def SPA_partition_function_raw(UINT32_t [:,:] index_matrix, FLOAT32_t [:,:] acc_
             Z[j] = Z1
 
     return Z.base
+
+
+# @cython.boundscheck(False)
+# @cython.wraparound(False)
+# @cython.initializedcheck(False)
+# @cython.cdivision(True)
+# @cython.overflowcheck(False)
+# def SPA_gradient_raw(UINT32_t [:,:] index_matrix, FLOAT32_t [:,:] acc_matrix, FLOAT32_t [:] kmer_invkd, UINT64_t k, int n_max=0, int openen_ofs=0):
+#     assert k <= 16 # must fit into UINT32 kmer-indices!
+
+#     cdef UINT64_t N = index_matrix.base.shape[0]
+#     cdef UINT64_t l = index_matrix.base.shape[1]
+#     cdef UINT64_t n_k = 4**k
+#     # result will be stored here (Z = 'Zustandssumme' sum of states)
+#     cdef FLOAT32_t [:] Z = np.empty(N, dtype=np.float32)
+    
+#     # Jacobi matrix, how affinity of each kmer will change the weighted of each kmer in the pulldown
+#     cdef FLOAT32_t [:,:] J = np.zeros((n_k,n_k), dtype=np.float32)
+
+#     # accumulated kmer Boltzmann weights from all reads that contain them
+#     cdef FLOAT32_t [:] W = np.zeros(n_k, dtype=np.float32)
+
+#     # helper variables to tell cython the types
+#     cdef FLOAT32_t a=0
+#     cdef UINT64_t i=0, j=0
+#     cdef UINT32_t index=0
+#     cdef FLOAT32_t w=0
+#     cdef FLOAT64_t Z1=0 # Single protein partition function
+
+#     if n_max:
+#         N = min(N, n_max)
+
+#     with nogil, parallel():
+#         for j in prange(N, schedule='guided'):
+#             Z1 = 0
+#             # iterate over all k-mers in this read
+#             for i in range(0, l):
+#                 # assigned variables are thread-local
+#                 index = index_matrix[j, i]
+#                 a = acc_matrix[j, i + openen_ofs]
+#                 w = kmer_invkd[index] * a
+#                 Z1 = Z1 + w
+
+#             # re-iterate to update Jacobi matrix and weighted kmer table
+#             for i in range(0, l):
+#                 index = index_matrix[j, i]
+#                 a = acc_matrix[j, i + openen_ofs]
+#                 w = kmer_invkd[index] * a
+#                 Z1 = Z1 + w
+            
+#             Z[j] = Z1
+
+#     return Z.base
 
 
 
