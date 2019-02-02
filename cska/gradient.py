* fixed bugs in mparams gradient computation. analytical gradient seems correct now.

cska/gradient.py
@@ -17,9 +17,10 @@ class Tracked(object):
 
 
 def emp_grad(state, eps=1e-6):
-    v0 = state.params.as_vector()
+    v0 = state.params.get_data()
+    v = np.array(v0)
     var = state.params.copy()
-    grad = state.params.copy()
+    grad = np.array(v0)
     state0 = state
     # print "err0", err0
     kw = dict()#.predict_kwargs)
@@ -27,19 +28,22 @@ def emp_grad(state, eps=1e-6):
     kw['tune'] = False
     kw['rbp_free'] = state0.rbp_free
 
-    for i in range(state.params.n):
+    for i in range(len(v0)):
         # print ">>> EMP GRAD", state.params.names[i]
         # d = max(v0[i] * eps,1e-6)
         d = eps
-        var.data[i] = v0[i] + d
+        v[i] = v0[i] + d
+        var.set_data(v)
         state = state.mdl.predict(var, **kw)
         derr = state.error - state0.error
-        grad.data[i] = derr/d
+        grad[i] = derr/d
         # print "derr", derr
-        var.data[i] = v0[i]
+        v[i] = v0[i]
+
         # print ">>>GRAD ELEMENT", grad.data[i]
     
-    return grad
+    var.set_data(grad)
+    return var
 
 
 def emp_gradi(state, eps=1e-6):
@@ -299,12 +303,19 @@ class GradientDescent(object):
         try:
             while not self.converged() and self.t < self.maxiter and dt < self.maxtime:
                 print "computing gradient"
-                local_grad = state.grad.unity()
+                local_grad = state.grad #.unity()
                 if debug:
-                    print "LOCAL GRAD"
+                    print "LOCAL GRAD, EMP. GRAD"
                     print local_grad
-                # local_grad.A0 = 0
+                    # for lcl, emp in zip(local_grad, emp_grad(state)):
+                    #     print "LCL"
+                    #     print lcl
+                    #     print "EMP"
+                    #     print emp
+
+
                 local_grad.betas *= 0
+                # local_grad.A0 = 0
                 # local_grad = local_grad.unity()
                 descent = self.RMSprop( - local_grad ).unity()
                 # descent = self.momentum_grad( - local_grad).unity()

cska/params.py
@@ -174,13 +174,20 @@ class ModelSetParams(object):
     def apply_delta(self, delta_set):
         c = self.copy()
         new = []
-        for params, delta in zip(c.param_set, delta_set):
-            p = np.clip(params.psam_matrix + delta.psam_matrix, 1e-6, None)
-            M = p.max(axis=1)
+        for i, (params, delta) in enumerate(zip(c.param_set, delta_set)):
+            p = params.psam_matrix + delta.psam_matrix
+            # print i, "after applying update of magnitude", np.fabs(delta.data).max(), "min/max", p.min(), p.max()
+            m = p.min(axis=1) # find out if we dropped below zero
+            m = np.where(m < 0, -m + 1e-6, 0)
+            # print "raise", m
+            p += m[:, np.newaxis] # and raise the level in these columns accordingly
+            p = np.clip(p, 1e-6, None)
+            M = p.max(axis=1) # increases above 1 on cognate should increase A0
             p /= M[:,np.newaxis]
             params.psam_matrix = np.clip(p, 1e-6, 1)
 
             params.A0 *= M.prod() # keep matrix elements <= 1 and absorb excess into A0
+            # print i, "increasing A0 by", M.prod()
             params.A0 = max(1e-6, params.A0 + delta.A0) # prevent underflow
 
             params.betas = np.clip(params.betas + delta.betas, 1e-9, None)

cska/partfunc.py
@@ -32,18 +32,20 @@ class PartFuncModelState(object):
                 openen_ofs=self.mdl.openen.ofs - self.mdl.k_mdl + 1 + self.mdl.acc_shift
             )
             Z1_read, Z1_read_max = cyska.clipped_sum_and_max(Z1, clip=1E6)
-            self.Z1_motif.append(Z1)
             self.Z1_read_motif.append(Z1_read)
-
+        
             if self.Z1 is None:
                 self.Z1 = Z1
+                self.Z1_motif.append(Z1)
                 self.Z1_read = Z1_read
                 self.Z1_read_max = Z1_read_max
             else:
                 A_rel = par.A0/self.params.A0 # additional motif affinities are relative to motif0.A0!
-                self.Z1 += Z1 * A_rel
-                self.Z1_read += Z1_read * A_rel
-                self.Z1_read_max += Z1_read_max * A_rel
+                Zscaled = Z1 * A_rel
+                self.Z1_motif.append(Zscaled)
+                self.Z1 = self.Z1 + Zscaled
+                self.Z1_read = self.Z1_read + Z1_read * A_rel
+                self.Z1_read_max = self.Z1_read_max + Z1_read_max * A_rel
                 # TODO: extend clipped_sum_and_max to handle max properly
 
         # vector_stats(self.Z1_read)
@@ -99,7 +101,7 @@ class PartFuncModelState(object):
         self.R_errors = np.array(self.R, dtype=np.float64) - self.mdl.R0
         self.sample_errors = (self.R_errors**2).mean(axis=1)
         self.error = self.sample_errors.mean()
-        print "self.error", self.error
+        # print "self.error", self.error
 
 
     @property
@@ -169,7 +171,7 @@ class PartFuncModelState(object):
         # self.E_weights = np.ones(self.mdl.nA, dtype=np.float32) + 10 * self.kmer_affinity_weights(cutoff=.01)
         # self.E_weights /= self.E_weights.mean()
         from cska.params import ModelSetParams
-        grad_set = [cyska.PSAM_partition_function_gradient(self, par, Z1) for par, Z1 in zip(self.params, self.Z1_motif)]
+        grad_set = [cyska.PSAM_partition_function_gradient(self, par, Z1m) for par, Z1m in zip(self.params, self.Z1_motif)]
         _grad = ModelSetParams(grad_set)
         # _grad.A0 *= 0
         # _grad.betas *= 0