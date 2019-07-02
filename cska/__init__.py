c003084d4adca718de9c4d91f1e24a6d2a299ac1
Marvin Jens <mjens@mit.edu>
2019-07-02 16:47:27 -0400

    * removed -a and made auto_detect() default behaviour if no files are given. Changed seed switches back to k=8 and higher thresholds (needs testing)

--- cska/__init__.py
+++ cska/__init__.py
@@ -27,8 +27,8 @@ def parse_cmdline():
     parser.add_option("","--name", dest="name", default="RBP", help="name of the protein assayed (default=RBP)")
     parser.add_option("-o","--output", dest="output", default="cska", help="path where results are to be stored (default='cska')")
     parser.add_option("","--run-path", dest="run", default="run_{datestr}", help="pattern for run-folder name (default='run_{datestr}')")
-    parser.add_option("-a","--auto", dest="auto", default=False, action="store_true", help="SWITCH: attempt to automatically guess RPB name, reads files and concentrations from file names (default=specify manually)")
-    parser.add_option("-b","--best", dest="best", default=0, type=int, help="keep only the best n samples (by top R-value) default=0 [off]")
+    # parser.add_option("-a","--auto", dest="auto", default=False, action="store_true", help="SWITCH: attempt to automatically guess RPB name, reads files and concentrations from file names (default=specify manually)")
+    parser.add_option("-b","--best", dest="best", default=3, type=int, help="keep only the best n samples (by top R-value) default=4 [0=take all]")
     parser.add_option("","--rank", dest="rank", default=None, type=int, help="analyze x out of the n --best samples (by top R-value) default=None [off]")
     parser.add_option("","--resume", dest="resume", default=False, action="store_true", help="re-use previous results")
     parser.add_option("","--redo", dest="redo", default=False, action="store_true", help="do not re-use previous results at all")
@@ -60,7 +60,7 @@ def parse_cmdline():
     parser.add_option("","--subsamples",dest="subsamples",default=10,type=int,help="number of subsamples for error estimation (default=10)")
 
     # seed motif analysis
-    parser.add_option("","--seed-k",dest="k_seed",default=7, type=int, help="kmer size used for seeding PSAM(s) (default=7)")
+    parser.add_option("","--seed-k",dest="k_seed",default=8, type=int, help="kmer size used for seeding PSAM(s) (default=8)")
 
     # accessibility footprint analysis
     parser.add_option("","--footprint-k", dest="footprint", default="5-12", help="size range [nt] to search for ideal accessibility footprint (default: --footprint-k=5-12)")
@@ -77,7 +77,7 @@ def parse_cmdline():
     parser.add_option("", "--opt-seed", dest="opt_seed", default=False, action="store_true", help="perform initial motif construction (STAGE0: seed-stage)")
     parser.add_option("", "--z-cut", dest="z_cut", default=4., type=float, help="Z-score cutoff for R-values of kmers that go into motif building (default=4)")
     parser.add_option("", "--max-motifs", dest="max_motifs", default=5, type=int, help="maximal number of individual PSAMs (variant motifs) being fitted (default=5)")
-    parser.add_option("", "--seed-thresh", dest="seed_thresh", default=.72, type=float, help="score threshold for k-mer:PSAM alignment to trigger a new PSAM (default=.72)")
+    parser.add_option("", "--seed-thresh", dest="seed_thresh", default=.8, type=float, help="score threshold for k-mer:PSAM alignment to trigger a new PSAM (default=.72)")
     parser.add_option("-w","--max-width",dest="max_width",default=11, type=int, help="maximum number of nucleotides in PSAM motif (number of columns) default=11)")
 
     parser.add_option("", "--opt-nostruct", dest="opt_nostruct", default=False, action="store_true", help="perform no-struct gradient descent (STAGE1: nostruct stage)")
@@ -200,7 +200,7 @@ class Run(object):
     def __init__(self, options, args):
         self.options = options
         self.args = args
-        if options.auto:
+        if not args:
             self.rbp_name, self.reads_files, self.rbp_concentrations = auto_detect('.')
         else:
             self.rbp_name = options.name
@@ -498,7 +498,11 @@ class Run(object):
         # TODO!
         # SR.primer_analysis()
         # sys.exit(0)
-        self.params = SR.seeded_multi_params(self.rbns.n_samples, max_motifs=self.options.max_motifs, k_seed=self.options.k_seed, thresh=self.options.seed_thresh, z_cut=self.options.z_cut)
+        a5 = self.rbns.reads[0].adap5
+        a3 = self.rbns.reads[0].adap3 
+        # contaminants = [a5, a3, rev_comp(a5), rev_comp(a3)]
+        contaminants = []
+        self.params = SR.seeded_multi_params(self.rbns.n_samples, max_motifs=self.options.max_motifs, k_seed=self.options.k_seed, thresh=self.options.seed_thresh, z_cut=self.options.z_cut, contaminants=contaminants)
         self.params.save(os.path.join(self.run_path, 'seed/initial.tsv'))
         
 
