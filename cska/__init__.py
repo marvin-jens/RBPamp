74fdaa8c2fc2fa32bcb956b3e0e9fb121acca5df
Marvin Jens <mjens@mit.edu>
2019-07-09 17:22:51 -0400

    * update default values

--- cska/__init__.py
+++ cska/__init__.py
@@ -43,8 +43,8 @@ def parse_cmdline():
     parser.add_option("","--format", dest="format", default='raw', help="read file format [raw,fasta,fastq] (default=raw)")
     parser.add_option("","--adap5", dest="adap5", default="gggaguucuacaguccgacgauc", help="5'RNA adapter sequence to add to read sequence")
     parser.add_option("","--adap3", dest="adap3", default="uggaauucucgggugucaagg", help="3'RNA adapter sequence to add to read sequence")
-    parser.add_option("-N","--n-max", dest="n_max", default=10000000, type=int, help="read at most N reads (preserves RAM for very deep sequencing libraries. default=10M, 0=off)")
-    parser.add_option("-n","--n-samples", dest="n_samples", default=1000000, type=int, help="TESTING: sub-sample n reads from N reads")
+    parser.add_option("-N","--n-max", dest="n_max", default=15000000, type=int, help="read at most N reads (preserves RAM for very deep sequencing libraries. default=10M, 0=off)")
+    parser.add_option("-n","--n-samples", dest="n_samples", default=5000000, type=int, help="TESTING: sub-sample n reads from N reads")
     parser.add_option("-r","--resample-interval", dest="resample_int", default=5, type=int, help="TESTING: re-sample every -r iterations of descent (default=5, 0 to disable)")
     parser.add_option("","--no-replace", dest="replace", default=True, action="store_true", help="TESTING: disable drawing with replacement")
 
@@ -79,7 +79,7 @@ def parse_cmdline():
     parser.add_option("","--linear-occ",dest="linear_occ",default=False, action="store_true",help="MODEL: pretend no saturation: occ = P/Kd")
 
     parser.add_option("", "--opt-seed", dest="opt_seed", default=False, action="store_true", help="perform initial motif construction (STAGE0: seed-stage)")
-    parser.add_option("", "--z-cut", dest="z_cut", default=4., type=float, help="Z-score cutoff for R-values of kmers that go into motif building (default=4)")
+    parser.add_option("", "--z-cut", dest="z_cut", default=5., type=float, help="Z-score cutoff for R-values of kmers that go into motif building (default=4)")
     parser.add_option("", "--max-motifs", dest="max_motifs", default=5, type=int, help="maximal number of individual PSAMs (variant motifs) being fitted (default=5)")
     parser.add_option("", "--seed-thresh", dest="seed_thresh", default=.8, type=float, help="score threshold for k-mer:PSAM alignment to trigger a new PSAM (default=.72)")
     parser.add_option("-w","--max-width",dest="max_width",default=11, type=int, help="maximum number of nucleotides in PSAM motif (number of columns) default=11)")
