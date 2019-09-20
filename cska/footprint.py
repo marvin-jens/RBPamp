12273bcc6008ce83bd6161dbdf4f9dd1d5b704a0
Marvin Jens <mjens@mit.edu>
2019-09-20 15:25:06 -0400

    * don't store altered A0 in footprint calibrated footprint (it's weirdly around 1 anyway?)

--- cska/footprint.py
+++ cska/footprint.py
@@ -404,7 +404,7 @@ class FootprintCalibration(CachedBase):
         self.params.acc_shift = s
         self.params.acc_scale = a
         self.params.rel_err = rel_err
-        self.params.A0 = A0
+        # self.params.A0 = A0
         self.params.rbp_name = self.input_reads.rbp_name
         self.store_shelve("params_calibrated", self.params)
 
