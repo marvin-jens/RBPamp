5555c998b0e8faeaf52ef8a53d3d3b0650ce795a
Marvin Jens <mjens@mit.edu>
2019-10-03 11:17:45 -0400

    * allow to get partition function for single PSAMs out ouf a set

--- cska/util.py
+++ cska/util.py
@@ -5,7 +5,7 @@ def load_model(fname):
     params = ModelSetParams.load(fname, 1)
     return params
 
-def eval_model(params, seq):
+def eval_model(params, seq, m=None):
     if len(seq) < params.k:
         return np.zeros(len(seq), dtype=np.float32)
     from cska.cyska import seq_to_bits, PSAM_partition_function
@@ -16,9 +16,14 @@ def eval_model(params, seq):
     accm = np.ones(seqm.shape, dtype=np.float32)
     # print accm, accm.dtype
     # print "seqm", seqm.shape, seqm.min(), seqm.max()
-    Z = np.array([PSAM_partition_function(seqm, accm, par.psam_matrix, single_thread=True) * par.A0/params.A0 for par in params])
-    # print Z.shape, "lseq", len(seq)
-    return Z.sum(axis=0)[0, :]  # one sequence, sum over all sub-motifs
+    if m is None:
+        Z = np.array([PSAM_partition_function(seqm, accm, par.psam_matrix, single_thread=True) * par.A0/params.A0 for par in params])
+        # print Z.shape, "lseq", len(seq)
+        return Z.sum(axis=0)[0, :]  # sum over all sub-motifs. we have only one sequence->index 0
+    else:
+        par = params.param_set[m]
+        Z = PSAM_partition_function(seqm, accm, par.psam_matrix, single_thread=True) * par.A0/params.A0
+        return Z[0, :]
 
 def motif_peaks(Z, Amin=1e-3, pad=50, k=8):
     i = Z.argmax()
