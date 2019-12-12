4e8e25c7b91740d10dba9fbf7e81d90ad6528d69
Marvin Jens <mjens@mit.edu>
2019-12-12 15:48:46 -0500

    * remove git_commit newline

--- RBPamp/__init__.py
+++ RBPamp/__init__.py
@@ -10,7 +10,7 @@ def git_commit():
     path = os.path.dirname(os.path.realpath(__file__))
     gc_name = os.path.join(path, "../git_commit")
     if os.path.exists(gc_name):
-         return open(gc_name, 'r').read()
+         return open(gc_name, 'r').read().rstrip()
     else:
         try:
             git = subprocess.Popen(["git","describe","--always"], cwd=path, stdout=subprocess.PIPE).communicate()[0].rstrip()
