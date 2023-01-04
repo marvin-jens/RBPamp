0c686bd501e0ed5ea3ae25d7983f244c3640aa7a
Marvin Jens <marvin.jens@charite.de>
2023-01-05 00:16:14 +0100

    * added known_kds.csv to the package

--- setup.py
+++ setup.py
@@ -96,6 +96,8 @@ setup(
     entry_points={'console_scripts': ['RBPamp = RBPamp.cmdline:main']},
     package_dir='',
     packages=['RBPamp'],
+    package_data={'': ['known_kds.csv']},
+    include_package_data=True,
     cmdclass = cmdclass,
     ext_modules=ext_modules,
 )
